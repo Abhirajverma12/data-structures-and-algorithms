807785567cdc8506bdf48c36429e3ca1505a1486
ABHIRAJ VERMA <abhirajverma0133@gmail.com>
2026-06-22 18:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (8.36%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,25 +11,49 @@
  */
 class Solution {
 public:
-//  Solve the Inorder traveersal by ITERATIVE way without recursion
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(root == NULL) return ans;
-        stack<TreeNode*>st ;
-        
-        while(true){
-            if(root != NULL){ 
-                st.push(root);
-                root = root->left; 
+//  flag -- Preorder =1 , Inorder = 2 ,Postorder =3 
+    vector<int> PreInPostTraversal(TreeNode * root,int flag){
+         if(root == NULL) return {};
+        vector<int>In,Pre,Post;
+        stack<pair<TreeNode*, int>>st;
+        st.push({root,1});
+
+        while(!st.empty()){
+            auto it = st.top();
+            st.pop();
+            TreeNode* node = it.first;
+            int num = it.second;
+
+            if(num ==1 ){
+                Pre.push_back(node->val);
+                num++;
+                st.push({node,num});
+                if(node->left != NULL ){
+                    st.push({node->left,1});
+                }
             }
-            else{ // if our node is NULL 
-                if(st.empty()) break; // this to terminate from the while loop after process all tthe elements
-                root = st.top();
-                st.pop();
-                ans.push_back(root->val);
-                root = root->right;
+            else if(num ==2){
+                In.push_back(node->val);
+                num++;
+                st.push({node,num});
+                if(node->right!= NULL){
+                    st.push({node->right,1});
+                }
+            }
+            else {
+                Post.push_back(node->val);
+                
             }
         }
-        return ans;
+        if(flag ==1) return Pre;
+        if(flag == 2) return In;
+        return Post;
+
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+       
+        vector<int>Inorder = PreInPostTraversal(root,2);
+        return Inorder ;
     }
 };
